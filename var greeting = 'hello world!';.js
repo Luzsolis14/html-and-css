8fb65d09f061a7204c08c55fe45a8bd611d69ff7
Luzsolis14 <90864462+Luzsolis14@users.noreply.github.com>
2021-10-19 09:51:54 -0700

    new files

---
+++ var greeting = 'hello world!';.js
@@ -0,0 +1,8 @@
+var greeting = 'hello world!';
+console.log(greeting);;
+
+var greeting2 = 'Welcome to Noode.js';
+console.log(greeting2);
+
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentenceOne);
